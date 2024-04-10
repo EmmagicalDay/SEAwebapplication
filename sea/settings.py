ce195a508e00062c6269072bf25d28401b36040b
EmmagicalDay <45723381+EmmagicalDay@users.noreply.github.com>
2024-04-10 01:35:56 +0100

    Update settings.py

--- sea/settings.py
+++ sea/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1+38pky@f2=bu-+(8smk2bdz#n5lg2jkr14(^g%1d0wt2j&ofm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
